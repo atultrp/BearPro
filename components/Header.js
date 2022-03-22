10239f488f9d714fd513432334c432f0993cc55f
Atul Tripathi <atul31815@gmail.com>
2022-03-22 11:56:57 +0530

    Initial setup

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+
+const Header = () => {
+    return (
+        <>
+            <nav className="flex items-center justify-between py-6 px-16">
+                <h1 className="text-2xl">MIBTEST PRO</h1>
+                <ul className="flex space-x-16">
+                    <li>Home</li>
+                    <li>Mint</li>
+                    <li>Team</li>
+                    <li>FAQ</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Header
