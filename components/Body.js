33d94b93110c01a4670ebdb4c20baf1505bdd417
Atul Tripathi <atul31815@gmail.com>
2022-03-22 23:00:41 +0530

    commit

--- components/Body.js
+++ components/Body.js
@@ -1,9 +1,10 @@
+import Image from "next/image"
 
 const Body = () => {
     return (
         <div>
-            <div id="home" className="pt-16 mx-3.5 lg:w-1/2 md:mx-auto">
-                <h2 className="text-4xl md:text-6xl md:text-center custom-font uppercase leading-tight font-bold">
+            <div id="home" className="pt-16 mx-3.5 lg:w-1/2 md:mx-auto ">
+                <h2 className="text-4xl md:text-6xl md:text-center custom-font uppercase md:leading-snug font-bold">
                     Collect MIB NFTs on the metaverse
                 </h2>
                 <div className="w-11/12 md:w-10/12 mx-auto">
@@ -11,11 +12,67 @@ const Body = () => {
                         3000 confused bears who have somehow made their way into the metaverse. Only on Solana.
                     </p>
                     <div className="flex md:w-1/2 md:mx-auto space-x-3 pt-7">
-                        <button className="custom-font py-2 px-4 text-lg bg-red-500 text-white rounded-full uppercase font-semibold hover:bg-opacity-75">Mint a MIB</button>
+                        <button className="custom-font py-2 px-4 text-lg bg-red-500 text-white rounded-full uppercase font-semibold hover:bg-opacity-75">
+                            <a href="#mint">Mint a MIB</a>
+                        </button>
                         <button className="custom-font py-2 px-4 text-lg bg-blue-500 text-white rounded-full uppercase font-semibold hover:bg-opacity-75">Marketplace</button>
                     </div>
                 </div>
             </div>
+
+            {/* In Progress */}
+            <div className="container mx-auto w-full overflow-hidden relative pt-16">
+                <div className="flex space-x-4 before:bg-gradient-to-r from-indigo-500 ">
+                    <div className="">
+                        <Image src="/Assets/1.png" width={200} height={200} />
+                    </div>
+                    <div className="">
+                        <Image src="/Assets/2.png" width={200} height={200} />
+                    </div>
+                    <div className="">
+                        <Image src="/Assets/3.png" width={200} height={200} />
+                    </div>
+                    <div className="">
+                        <Image src="/Assets/4.png" width={200} height={200} />
+                    </div>
+                    <div className="">
+                        <Image src="/Assets/5.png" width={200} height={200} />
+                    </div>
+                    <div className="">
+                        <Image src="/Assets/6.png" width={200} height={200} />
+                    </div>
+                    <div className="">
+                        <Image src="/Assets/7.png" width={200} height={200} />
+                    </div>
+                    <div className="">
+                        <Image src="/Assets/8.png" width={200} height={200} />
+                    </div>
+                    <div className="">
+                        <Image src="/Assets/9.png" width={200} height={200} />
+                    </div>
+                </div>
+            </div>
+
+            <div id="mint" className="pt-16">
+                <h2 className="text-4xl md:text-6xl md:text-center custom-font uppercase md:leading-snug font-bold">
+                    Mint a MIB
+                </h2>
+                <div className="lg:flex">
+                    <div className="">
+                        <img src="/Assets/beargif.gif" />
+                    </div>
+                    <div className="pl-6">
+                        <h3 className="custom-font text-3xl font-bold text-[#aa9b76] uppercase">About</h3>
+                        <p className="mx-4 text-lg">
+                            An NFT, also known as Non-Fungible Token, is a one-of-a-kind digital token stored on a digital ledger/blockchain. The ther "Non-Fungible" signifies something not interchangeable with another good due to its distinct properties.<br />
+                            The Mibbear collection is made up of 3000 NFTs, each NFT has different characteristics that make each of them totally unique. Depending on the different characteristics that an nft obtains, it will have a different level of rarity. For example, only 30 nfts will have the paladin helmet, giving them a mythic rarity.<br />
+                            Each rarity gives you a different weight in the project. How to participate in project decisions, or airdrops of our token.
+                        </p>
+                        <button className="custom-font py-2 px-4 text-lg bg-red-500 text-white rounded-full uppercase font-semibold hover:bg-opacity-75">Connect And Mint</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
